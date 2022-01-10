5e5f07e8ed773950c8b03b64fa325e4f258b4c99
diegoquispe-dvp <66976983+diegoquispe-dvp@users.noreply.github.com>
2022-01-10 16:21:42 -0500

    first commit

---
+++ Assets/Scripts/PlayerLogic.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerLogic : MonoBehaviour
+{
+    public float velocity = 1; //velocidad del personaje
+    private Rigidbody2D rb; // la física del personaje
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>(); //Se extrae la física del componente (personaje)
+        
+    }
+
+    
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            rb.velocity = Vector2.up * velocity; //se toma la variable con la física del personaje
+            
+            Debug.Log(Vector2.up);
+            Debug.Log(rb.velocity);
+            Debug.Log(velocity);
+        }
+    }
+    private void OnCollisionEnter2D(Collision2D collision) {
+        
+    }
+}
